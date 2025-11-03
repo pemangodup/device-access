35348efda747dca0d24e47934e5192e7add177cc
Pema Ngodup <pngodup123@gmail.com>
2025-11-03 17:10:31 +0000

    OutlinedButton button added

--- components/Place/ImagePicker.js
+++ components/Place/ImagePicker.js
@@ -6,6 +6,7 @@ import {
 } from "expo-image-picker";
 import { useState } from "react";
 import { Colors } from "../../constants/colors";
+import OutlinedButton from "../UI/OutlinedButton";
 
 function ImagePicker() {
   const [pickedImage, setPickedImage] = useState();
@@ -52,7 +53,9 @@ function ImagePicker() {
   return (
     <View>
       <View style={styles.imagePreview}>{imagePreview}</View>
-      <Button title="Take Image" onPress={takeImageHandler} />
+      <OutlinedButton icon="camera" onPress={takeImageHandler}>
+        Take Image
+      </OutlinedButton>
     </View>
   );
 }
