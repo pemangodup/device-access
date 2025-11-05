cbfb2cd999a44e5fc8000ec9a5e3d0187babaa53
Pema Ngodup <pngodup123@gmail.com>
2025-11-05 10:51:41 +0000

    Map location picked and marker selected

--- screens/Map.js
+++ screens/Map.js
@@ -1,14 +1,38 @@
+import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 
 function Map() {
+  const [selectedLocation, setSelectedLocation] = useState();
   const region = {
     latitude: 37.78,
     longitude: -122.43,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0422,
   };
-  return <MapView style={styles.map} initialRegion={region}></MapView>;
+
+  function selectLocationHandler(event) {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+    setSelectedLocation({ lat: lat, lng: lng });
+  }
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          title="Picked Location"
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
+  );
 }
 
 export default Map;
@@ -16,5 +40,6 @@ export default Map;
 const styles = StyleSheet.create({
   map: {
     flex: 1,
+    margin: 1,
   },
 });
