94113ea67aaa4d0e769f355140f198f8132a4036
Pema Ngodup <pngodup123@gmail.com>
2025-11-11 10:45:01 +0000

    Outputting a list of places

--- models/place.js
+++ models/place.js
@@ -2,7 +2,6 @@ export class Place {
   constructor(title, imageUri, location) {
     this.title = title;
     this.imageUri = imageUri;
-    this.address = location.address;
     this.location = { lat: location.lat, lng: location.lng }; // {lat: 0.383883, lng: 9.7373}
     this.id = new Date().toString() + Math.random().toString();
   }
--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,8 +1,19 @@
 import { Text } from "react-native";
 import Placeslist from "../components/Place/PlacesList";
+import { useEffect, useState } from "react";
+import { useIsFocused } from "@react-navigation/native";
 
-function AllPlaces() {
-  return <Placeslist />;
+function AllPlaces({ route }) {
+  const [loadedPlaces, setLoadedPlaces] = useState([]);
+
+  const isFocused = useIsFocused();
+  useEffect(() => {
+    if (isFocused && route.params) {
+      setLoadedPlaces((curPlaces) => [...curPlaces, route.params.place]);
+    }
+  }, [isFocused, route]);
+
+  return <Placeslist places={loadedPlaces} />;
 }
 
 export default AllPlaces;
