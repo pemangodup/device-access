84a1024753a141ba81b7560021f64219afe4b62b
Pema Ngodup <pngodup123@gmail.com>
2025-11-12 10:51:43 +0000

    Working on with locol database with expo sqlite

---
+++ util/database.js
@@ -0,0 +1,6 @@
+import * as SQLite from "expo-sqlite";
+
+const database = SQLite.openDatabaseAsync("place.db");
+export function init() {
+  const promise = new Promise((resolvle, reject) => {});
+}
